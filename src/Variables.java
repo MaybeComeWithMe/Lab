4144004b137db75f5db08cfca5db83dcd838601c
Dmitry Marchuk <dmmrch@gmail.com>
2019-10-20 13:34:42 +0300

    init_commit

---
+++ src/Variables.java
@@ -0,0 +1,82 @@
+public class Variables {
+//
+//    boolean, char, byte, short, int, long, float, long.
+//    private int i = 0;
+//    private long l = 1L;
+//    private byte b = 0x07;
+//    private short s = 2;
+//    private float f = 3.3f;
+//    private double d = 1.0;
+//    private boolean swit = true;
+//    private char с = 'asd';
+//
+//    public int getI() {
+//
+//        return i;
+//    }
+//
+//    public void setI(int i) {
+//
+//        this.i = i;
+//    }
+//
+//    public long getL() {
+//
+//        return l;
+//    }
+//
+//    public void setL(long l) {
+//
+//        this.l = l;
+//    }
+//
+//    public byte getB() {
+//
+//        return b;
+//    }
+//
+//    public void setB(byte b) {
+//
+//        this.b = b;
+//    }
+//
+//    public short getS() {
+//
+//        return s;
+//    }
+//
+//    public void setS(short s) {
+//
+//        this.s = s;
+//    }
+//
+//    public float getF() {
+//
+//        return f;
+//    }
+//
+//    public void setF(float f) {
+//
+//        this.f = f;
+//    }
+//
+//    public double getD() {
+//
+//        return d;
+//    }
+//
+//    public void setD(double d) {
+//
+//        this.d = d;
+//    }
+//
+//    public boolean isSwit() {
+//
+//        return swit;
+//    }
+//
+//    public void setSwit(boolean swit) {
+//
+//        this.swit = swit;
+//    }
+}
